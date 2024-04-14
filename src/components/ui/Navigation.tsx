b398f2ab643555822a91afa77cd4b7d33c60e633
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-04-14 23:49:16 +0700

    update

--- src/components/ui/Navigation.tsx
+++ src/components/ui/Navigation.tsx
@@ -36,41 +36,41 @@ export const htmlCode = `
 <nav className="max-w-[300px] w-full">
   <ul>
     // Active: "bg-gray-100"
-    <li className="mail-folder__menu flex items-center gap-4 py-3 px-4 rounded transition-all active">
+    <li className="flex items-center gap-4 py-3 px-4 rounded transition-all active">
       // Active: "text-violet-400"
       <Mail size={33} className="p-2 bg-gray-100 rounded" />
       <h3 className="text-base">Inbox</h3>
       <div className="ml-auto text-xs">
         <span className="text-gray-500">+6</span>
         // Active: "bg-violet-400 text-white"
-        <span className="mail-folder__menu-count inline-block ml-2 p-2 bg-gray-100 rounded-full">
+        <span className="inline-block ml-2 p-2 bg-gray-100 rounded-full">
           132
         </span>
       </div>
     </li>
-    <li className="mail-folder__menu flex items-center gap-4 py-3 px-4 rounded transition-all">
+    <li className="flex items-center gap-4 py-3 px-4 rounded transition-all">
       <Send size={33} className="p-2 bg-gray-100 rounded" />
       <h3 className="text-base">Send</h3>
       <div className="ml-auto text-xs">
-        <span className="mail-folder__menu-count inline-block ml-2 p-2 bg-gray-100 rounded-full">
+        <span className="inline-block ml-2 p-2 bg-gray-100 rounded-full">
           45
         </span>
       </div>
     </li>
-    <li className="mail-folder__menu flex items-center gap-4 py-3 px-4 rounded transition-all">
+    <li className="flex items-center gap-4 py-3 px-4 rounded transition-all">
       <Star size={33} className="p-2 bg-gray-100 rounded" />
       <h3 className="text-base">Starred</h3>
       <div className="ml-auto text-xs">
-        <span className="mail-folder__menu-count inline-block ml-2 p-2 bg-gray-100 rounded-full">
+        <span className="inline-block ml-2 p-2 bg-gray-100 rounded-full">
           45
         </span>
       </div>
     </li>
-    <li className="mail-folder__menu flex items-center gap-4 py-3 px-4 rounded transition-all">
+    <li className="flex items-center gap-4 py-3 px-4 rounded transition-all">
       <File size={33} className="p-2 bg-gray-100 rounded" />
       <h3 className="text-base">Draft</h3>
       <div className="ml-auto text-xs">
-        <span className="mail-folder__menu-count inline-block ml-2 p-2 bg-gray-100 rounded-full">
+        <span className="inline-block ml-2 p-2 bg-gray-100 rounded-full">
           45
         </span>
       </div>
@@ -89,7 +89,7 @@ export const Navigation = () => {
               // Active: "bg-gray-100" */}
               <li
                 key={title}
-                className={`mail-folder__menu flex items-center gap-4 py-3 px-4 rounded transition-all ${
+                className={`flex items-center gap-4 py-3 px-4 rounded transition-all ${
                   active ? 'active' : ''
                 }`}
               >
@@ -101,7 +101,7 @@ export const Navigation = () => {
                     <span className="text-gray-500">+{unread}</span>
                   ) : null}
                   {/* Active: "bg-violet-400 text-white" */}
-                  <span className="mail-folder__menu-count inline-block ml-2 p-2 bg-gray-100 rounded-full">
+                  <span className="inline-block ml-2 p-2 bg-gray-100 rounded-full">
                     {count}
                   </span>
                 </div>
