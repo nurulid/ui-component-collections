2ba83ca24f22d634feac5fa473882bea6e384c29
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-04-16 21:49:22 +0700

    improve sidebar

--- src/components/share/Sidebar.tsx
+++ src/components/share/Sidebar.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 export const Sidebar = () => {
   return (
-    <section className='max-w-[250px] w-full p-10 sticky top-0 h-screen bg-griey'>Sidebar</section>
-  )
-}
+    <section className="max-w-[270px] w-full p-3 sticky top-0 h-screen">
+      <div className="rounded-md bg-griey h-full p-4">Sidebar</div>
+    </section>
+  );
+};
