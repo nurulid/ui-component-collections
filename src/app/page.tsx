53ac7abb5259b791db35f02137ff2995c1c3d919
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-04-16 22:04:03 +0700

    improve tab panel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,10 @@ import { CodeWrapper } from '@/components/ui/CodeWrapper';
 export default function Home() {
   return (
     <div className="p-10">
-      <h2 className="text-3xl mb-5">Tailwind UI</h2>
-      <div className="">
+      <h2 className="text-4xl mb-6">Tailwind UI</h2>
+      <div className="space-y-10">
         <TabPanel
+          title='Navigation'
           ui={<Navigation />}
           code={<CodeWrapper htmlCode={htmlNav} />}
           note={
--- src/components/ui/TabPanel.tsx
+++ src/components/ui/TabPanel.tsx
@@ -5,43 +5,53 @@ import * as Tabs from '@radix-ui/react-tabs';
 import { Code, Eye } from 'lucide-react';
 
 type Props = {
+  title: string;
   ui: JSX.Element;
   code: JSX.Element;
   note?: React.ReactNode;
 };
 
 export const TabPanel: React.FC<Props> = (props) => {
-  const { ui, code, note } = props;
+  const { title, ui, code, note } = props;
   return (
-    <Tabs.Root defaultValue="tab1">
-      <div className='flex justify-between flex-row-reverse mb-2'>
-        <Tabs.List className="text-xs space-x-2">
-          <Tabs.Trigger
-            value="tab1"
-            className="group data-[state=active]:text-black text-gray-500 hover:text-black hover:bg-indie/10 rounded-md transition-all p-2"
-          >
-            <Eye size={16} className="group-data-[state=active]:text-indie inline-block mr-1 -translate-y-[1px]" />
-            Preview
-          </Tabs.Trigger>
-          <Tabs.Trigger
-            value="tab2"
-            className="group data-[state=active]:text-black text-gray-500 hover:text-black hover:bg-indie/10 rounded-md transition-all p-2"
-          >
-            <Code size={16} className="group-data-[state=active]:text-indie inline-block mr-1 -translate-y-[1px]" />
-            Code
-          </Tabs.Trigger>
-        </Tabs.List>
-        <div>{note}</div>
+    <Tabs.Root defaultValue="tab1" className="bg-griey p-1 rounded-md">
+      <div className="mb-2 px-3 pt-2">
+        <h2 className='text-xl mb-1'>{title}</h2>
+        <div className="flex justify-between flex-row-reverse text-gray-500">
+          <Tabs.List className="text-xs space-x-2">
+            <Tabs.Trigger
+              value="tab1"
+              className="group data-[state=active]:text-black text-gray-500 hover:text-black hover:bg-indie/10 rounded-md transition-all p-2"
+            >
+              <Eye
+                size={16}
+                className="group-data-[state=active]:text-indie inline-block mr-1 -translate-y-[1px]"
+              />
+              Preview
+            </Tabs.Trigger>
+            <Tabs.Trigger
+              value="tab2"
+              className="group data-[state=active]:text-black text-gray-500 hover:text-black hover:bg-indie/10 rounded-md transition-all p-2"
+            >
+              <Code
+                size={16}
+                className="group-data-[state=active]:text-indie inline-block mr-1 -translate-y-[1px]"
+              />
+              Code
+            </Tabs.Trigger>
+          </Tabs.List>
+          <div>{note}</div>
+        </div>
       </div>
       <Tabs.Content
         value="tab1"
-        className="flex justify-center p-6 border-[0.5px] border-black/20 rounded-md empty:p-0 empty:border-none"
+        className="flex justify-center bg-white p-6 border-[0.5px] border-black/20 rounded-md empty:p-0 empty:border-none"
       >
         {ui}
       </Tabs.Content>
       <Tabs.Content
         value="tab2"
-        className="flex justify-center p-6 border-[0.5px] border-black/20 rounded-md empty:p-0 empty:border-none bg-white"
+        className="flex justify-center bg-white p-6 border-[0.5px] border-black/20 rounded-md empty:p-0 empty:border-none"
       >
         {code}
       </Tabs.Content>
