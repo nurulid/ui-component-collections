1c613006ebdd28278bdc366a341e7fac0c37eb22
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-04-17 14:14:21 +0700

    rename

--- src/components/share/Sidebar.tsx
+++ src/components/share/Sidebar.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 export const Sidebar = () => {
   return (
     <section className="max-w-[280px] w-full p-4 sticky top-0 h-screen">
-      <div className="rounded-md bg-griey h-full p-4 shadow-md">
-        <h2 className="mb-6 font-mono font-semibold">Tailwind UI</h2>
+      <div className="rounded-md bg-griey h-full p-4 border">
+        <h2 className="mb-6 font-mono font-semibold">Tailwind CSS UI</h2>
       </div>
     </section>
   );
