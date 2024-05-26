7fb4896eb02c9226d0417d989cf3abac0339edae
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-05-26 21:26:37 +0700

    feat: add pricing ui

---
+++ src/app/preview/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Pricing } from '../(content)/ui/components/Pricing'
+
+export default function Page() {
+  return (
+    <div>
+      <Pricing />
+    </div>
+  )
+}
