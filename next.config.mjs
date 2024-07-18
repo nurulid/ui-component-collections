650b07bd739bc2f84509e14e8be81bee40e948c1
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-07-18 16:46:32 +0700

    setup mdx

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import remarkGfm from "remark-gfm";
+import createMDX from "@next/mdx";
+import { remarkCodeHike } from "@code-hike/mdx";
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+};
+
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm, [remarkCodeHike, { theme: "css-variables" }]],
+  },
+});
+
+export default withMDX(nextConfig);
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './mdx-components.tsx',
   ],
   theme: {
     extend: {
@@ -29,6 +30,8 @@ const config: Config = {
   plugins: [
     plugin(function({ addVariant }) {
         addVariant('current', '&.active');
-    })],
+    }),
+    require("@tailwindcss/typography")
+  ],
 };
 export default config;
