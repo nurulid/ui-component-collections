0258b2407db0b4547677985b5d563216a568e63b
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-11-21 09:51:19 +0700

    up

--- src/components/share/Sidebar.tsx
+++ src/components/share/Sidebar.tsx
@@ -48,7 +48,7 @@ export const Sidebar = () => {
     <section className="max-w-[280px] w-full p-4 sticky top-0 h-screen">
       <div className="rounded-md bg-griey h-full p-4 border flex flex-col">
         <h2 className="mb-2 font-mono font-semibold border-b pb-2">
-          Tailwind CSS UI Library
+          UI Component Collections
         </h2>
         <p className='text-xs mb-10 text-gray-400'>Semantic, less &quot;div&quot;.</p>
         <ul className="space-y-1">
