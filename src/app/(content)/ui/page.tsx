aca71fe15d4d1f0125186a23c3d4e6437004e662
Nurul Insani Dewi <nurulinsanid@gmail.com>
2025-03-02 23:03:57 +0700

    page title

--- src/app/(content)/ui/page.tsx
+++ src/app/(content)/ui/page.tsx
@@ -12,45 +12,52 @@ import {
 
 export default function page() {
   return (
-    <div className="space-y-8">
-      <TabPanel
-        title="Navigation"
-        desc="Folder menu in dashboard navigation"
-        ui={<Navigation />}
-        codeHTML={<CodeWrapper code={htmlNav} />}
-        codeReact={<CodeWrapper code={reactNav} />}
-        note={
-          <p>
-            Icons:{' '}
-            <a href="https://lucide.dev/" target="_blank" className="note">
-              Lucide icon
-            </a>
-          </p>
-        }
-        bgContainer="bg-gray-50"
-      />
-      <TabPanel
-        title="Header"
-        desc="Simple header"
-        ui={<Header />}
-        codeHTML={<CodeWrapper code={htmlHeader} />}
-        codeReact={<CodeWrapper code={reactHeader} />}
-        note={
-          <p>
-            Icons:{' '}
-            <a href="https://lucide.dev/" target="_blank" className="note">
-              Lucide icon
-            </a>
-          </p>
-        }
-        bgContainer="bg-gray-50"
-      />
-      <TabPanel
-        title="X profile widget"
-        ui={<TwitterProfile />}
-        codeHTML={<CodeWrapper code={htmlProfile} />}
-        codeReact={<CodeWrapper code={reactProfile} />}
-      />
+    <div>
+      <div className="space-y-2">
+        <h1 className="text-3xl">UI Components</h1>
+        <p className="opacity-50">UI components for React.</p>
+      </div>
+      <div className="h-8" />
+      <div className="space-y-8">
+        <TabPanel
+          title="Navigation"
+          desc="Folder menu in dashboard navigation"
+          ui={<Navigation />}
+          codeHTML={<CodeWrapper code={htmlNav} />}
+          codeReact={<CodeWrapper code={reactNav} />}
+          note={
+            <p>
+              Icons:{' '}
+              <a href="https://lucide.dev/" target="_blank" className="note">
+                Lucide icon
+              </a>
+            </p>
+          }
+          bgContainer="bg-gray-50"
+        />
+        <TabPanel
+          title="Header"
+          desc="Simple header"
+          ui={<Header />}
+          codeHTML={<CodeWrapper code={htmlHeader} />}
+          codeReact={<CodeWrapper code={reactHeader} />}
+          note={
+            <p>
+              Icons:{' '}
+              <a href="https://lucide.dev/" target="_blank" className="note">
+                Lucide icon
+              </a>
+            </p>
+          }
+          bgContainer="bg-gray-50"
+        />
+        <TabPanel
+          title="X profile widget"
+          ui={<TwitterProfile />}
+          codeHTML={<CodeWrapper code={htmlProfile} />}
+          codeReact={<CodeWrapper code={reactProfile} />}
+        />
+      </div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,55 +4,72 @@ import { DockHover, reactDockHover } from '@/components/motion/DockHover';
 import { Pricing, reactPricing } from '@/components/motion/Pricing';
 import { Images, reactImages } from '@/components/ui/Images';
 import { Accordion, reactAccordion } from '@/components/motion/Accordion';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="space-y-8">
-      <TabPanel
-        title="Dock menu"
-        note={
-          <p>
-            Inspired by:{' '}
-            <a
-              href="https://x.com/jinyongftw/status/1739585482848342378"
-              target="_blank"
-              className="note"
-            >
-              x.com
-            </a>
-          </p>
-        }
-        ui={<DockHover />}
-        codeReact={<CodeWrapper code={reactDockHover} />}
-      />
-      <TabPanel
-        title="Image list"
-        note={
-          <p>
-            Inspired by:{' '}
-            <a
-              href="https://x.com/bartek_marzec/status/1798078019770482740"
-              target="_blank"
-              className="note"
-            >
-              x.com
-            </a>
-          </p>
-        }
-        ui={<Images />}
-        codeReact={<CodeWrapper code={reactImages} />}
-      />
-      <TabPanel
-        title="Accordion"
-        ui={<Accordion />}
-        codeReact={<CodeWrapper code={reactAccordion} />}
-      />
-      <TabPanel
-        title="Comparison Card"
-        note="Design source: Loom"
-        ui={<Pricing />}
-        codeReact={<CodeWrapper code={reactPricing} />}
-      />
-    </div>
+    <>
+      <div className="space-y-2">
+        <h1 className="text-3xl">Motion UI</h1>
+        <p className="opacity-50">
+          Using{' '}
+          <Link
+            href="https://www.npmjs.com/package/framer-motion"
+            className="underline"
+            target='_blank'
+          >
+            Framer motion.
+          </Link>
+        </p>
+      </div>
+      <div className="h-8" />
+      <div className="space-y-8">
+        <TabPanel
+          title="Dock menu"
+          note={
+            <p>
+              Inspired by:{' '}
+              <a
+                href="https://x.com/jinyongftw/status/1739585482848342378"
+                target="_blank"
+                className="note"
+              >
+                x.com
+              </a>
+            </p>
+          }
+          ui={<DockHover />}
+          codeReact={<CodeWrapper code={reactDockHover} />}
+        />
+        <TabPanel
+          title="Image list"
+          note={
+            <p>
+              Inspired by:{' '}
+              <a
+                href="https://x.com/bartek_marzec/status/1798078019770482740"
+                target="_blank"
+                className="note"
+              >
+                x.com
+              </a>
+            </p>
+          }
+          ui={<Images />}
+          codeReact={<CodeWrapper code={reactImages} />}
+        />
+        <TabPanel
+          title="Accordion"
+          ui={<Accordion />}
+          codeReact={<CodeWrapper code={reactAccordion} />}
+        />
+        <TabPanel
+          title="Comparison Card"
+          note="Design source: Loom"
+          ui={<Pricing />}
+          codeReact={<CodeWrapper code={reactPricing} />}
+        />
+      </div>
+    </>
   );
 }
