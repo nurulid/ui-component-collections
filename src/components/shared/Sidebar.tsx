7a214afbeccdc58482da55d8f3ffe899daf8ee36
Nurul Insani Dewi <nurulinsanid@gmail.com>
2025-03-04 17:22:40 +0700

    tweaks

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -53,7 +53,7 @@ const SidebarLink = (
 
 export const Sidebar = () => {
   return (
-    <section className="max-w-[280px] w-full p-4 sticky top-0 h-screen">
+    <section className="min-w-[280px] p-4 sticky top-0 h-screen">
       <div className="rounded-md bg-griey h-full p-4 border flex flex-col">
         <h2 className="mb-2 font-mono font-semibold border-b pb-2">
           UI Component Collections
